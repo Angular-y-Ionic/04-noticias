892a0d2ced762936dd352db0944b22884906ba64
Eduardo Gonzalez <eduardsalvadorgonzalez@gmail.com>
2020-11-30 18:34:24 -0600

    encabezadoss

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-
+export class Tab2Page implements OnInit{
+  categorias = ["business","entertainment","general","health","science","sports","technology"];
   constructor() {}
 
+  ngOnInit(){
+    // console.log(this.categorias[0]);
+    // this.segment.value = this.categorias[0];
+  }
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
+  }
 }
